3e4f7e5b68cfcd65071cc31712b91e24955945ca
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-08-30 14:33:02 +0800

    mod: The vue.config.js file

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,5 +1,26 @@
+const authApi = 'xxx';
+const commonApi = 'xxx';
+
 module.exports = {
   devServer: {
-    port: 8090
-  }
+    proxy: {
+      '/api/auth': {
+        target: authApi,
+        changeOrigin: true,
+        pathRewrite: {'/api/auth': '/auth'},
+      },
+      '/api/common': {
+        target: commonApi,
+        changeOrigin: true,
+        pathRewrite: {'/api/common': '/api'}
+      },
+    },
+    host: '0.0.0.0',
+    port: 8090,
+    https: false,
+  },
+  css: {
+    sourceMap: true,
+  },
+  productionSourceMap: false,
 }
